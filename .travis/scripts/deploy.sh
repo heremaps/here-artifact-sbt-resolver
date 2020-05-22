aa77ea79b5e7cf4f005da19f2cc84587c9f8bca0
Dmitry Abramov <dmitriy.abramov@here.com>
2020-05-22 16:17:02 +0200

    Clean up staging repos before release
    Signed-off-by: Dmitry Abramov <dmitriy.abramov@here.com>

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -12,5 +12,6 @@ openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_i
 gpg --fast-import .travis/codesigning.asc
 
 # Deploy to Maven Central
+sbt sonatypeDropAll
 sbt publishSigned
 sbt sonatypeRelease

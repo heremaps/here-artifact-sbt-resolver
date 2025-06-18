38df78d248539d79467563060bdb7d539a3c682f
Antno Wilhelm <anton.wilhelm@here.com>
2025-06-18 17:33:52 +0200

    SDKJAVA-424: Switch to Maven Central Portal
    Signed-off-by: Antno Wilhelm <anton.wilhelm@here.com>

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,8 +2,6 @@
 set -ev
 
 export GPG_TTY=$(tty)
-export SONATYPE_USERNAME=$OSSRH_USERNAME
-export SONATYPE_PASSWORD=$OSSRH_PASSWORD
 export PGP_PASSPHRASE=$GPG_PASSPHRASE
 
 gpg --version
@@ -15,4 +13,4 @@ gpg --import --batch private.key
 # Deploy to Maven Central
 sbt sonatypeDropAll
 sbt publishSigned
-sbt sonatypeRelease
+sbt sonatypeCentralRelease
